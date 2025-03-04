9cfaa522101ff1fa481c6e79b00e4ce9cc0b2081
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2025-03-04 21:32:15 +0500

    Conditinal program (if else)

--- conditional_loops.cpp
+++ conditional_loops.cpp
@@ -7,4 +7,17 @@ int main(){
 // cin >> age ;
 // cout << "Your age is : " << age ;
 // return 0 ;
-}
+
+// conditinal program (if else)
+// int age ;
+// cout << "Enter your age : "; 
+// cin >> age ;
+// if (age >= 18){
+// cout << "You are eligible for id card\n"  ;
+// }
+// else{
+//     cout << "You are not eligible for id card\n"  ;
+// }
+
+// return 0 ;
+// }
